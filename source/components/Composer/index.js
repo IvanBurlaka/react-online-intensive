72273c19a16b4bca2eb636b6768ca608bcdc654c
IvanBurlaka <ivanburlakas@gmail.com>
2019-02-13 23:09:46 +0200

    homework 4 done

--- source/components/Composer/index.js
+++ source/components/Composer/index.js
@@ -4,21 +4,27 @@ import React, { Component } from 'react';
 // Instruments
 import Styles from './styles.m.css';
 
+// Components
+import { Consumer } from '../HOC/withProfile'; 
+
 export default class Composer extends Component {
     render() {
-        const { avatar, currentUserFirstName } = this.props;
         return (
-            <section className={Styles.composer}>
-                <section>
-                    <img src={ avatar } />
-                    <form>
-                        <textarea 
-                            placeholder = { `What's on your mind, ${currentUserFirstName}?` }
-                        />
-                        <input type ="submit" value="Post"/>
-                    </form>
-                </section>            
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className={Styles.composer}>
+                        <section>
+                            <img src={ context.avatar } />
+                            <form>
+                                <textarea 
+                                    placeholder = { `What's on your mind, ${context.currentUserFirstName}?` }
+                                />
+                                <input type ="submit" value="Post"/>
+                            </form>
+                        </section>            
+                    </section>
+                )}
+            </Consumer>
         );
     }
 }
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,22 +4,22 @@ import moment from 'moment';
 
 import Styles from './styles.m.css';
 
+// Components
+import { Consumer } from '../HOC/withProfile'; 
+
 export default class Post extends Component {
     render() {
-
-        const {
-            currentUserFirstName,
-            currentUserLastName,
-            avatar
-        } = this.props;
-
         return (
-            <section className = { Styles.post }>
-                <img src={ avatar } />
-                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
-                <time>{moment().format('MMMM D h:mm:ss a')}</time>
-                <p>Howdy</p>
-            </section>
+            <Consumer>
+                {(context) => (
+                    <section className = { Styles.post }>
+                        <img src={ context.avatar } />
+                        <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
+                        <time>{moment().format('MMMM D h:mm:ss a')}</time>
+                        <p>Howdy</p>
+                    </section>
+                )}
+             </Consumer>
         );
     }
 }
