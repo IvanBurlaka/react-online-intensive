30372c9594df0a1ec942e3ae7a3c4a179321c52a
IvanBurlaka <ivanburlakas@gmail.com>
2019-02-13 22:59:24 +0200

    lesson 4 practice done

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 // Components
 import Feed from 'components/Feed';
+import { Provider } from 'components/HOC/withProfile';
 
 // Instruments
 import avatar from 'theme/assets/lisa';
@@ -15,6 +16,10 @@ const options = {
 
 export default class App extends Component {
     render() {
-        return <Feed { ...options } />;
+        return (
+        	<Provider value = { options }>
+        		<Feed/>
+    		</Provider>
+        )
     }
 }
