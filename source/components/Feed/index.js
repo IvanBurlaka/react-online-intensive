97198a5e21422f21a99575596ee9b5f4811060c9
Ivan Burlaka <ivan.b@dreamscapenetworks.com>
2019-02-25 18:21:55 +0200

    practice 6.2

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -9,47 +9,90 @@ import StatusBar from 'components/StatusBar';
 import Spinner from 'components/Spinner';
 
 //Instruments
-import Styles from './styles.m.css';
-import { getUniqueID } from 'instruments';
+import Styles from './styles.m.css'; 
+import { getUniqueID, delay } from 'instruments';
 
 export default class Feed extends Component {
 	constructor () {
 		super();
 
 		this._createPost = this._createPost.bind(this);
+        this._setPostsFetchingState = this._setPostsFetchingState.bind(this);
+        this._likePost = this._likePost.bind(this);
 	}
 	state = {
 		posts: [
-			{ id: '123', comment: 'Hi there!', created: 1550324484568 }, 
-			{id: '456', comment: 'wsaup biatch!', created: 1550324484568},
+			{ id: '123', comment: 'Hi there!', created: 1550324484568 , likes: []}, 
+			{id: '456', comment: 'wsaup biatch!', created: 1550324484568, likes: []},
 		],
-		isSpinning: true,
+		isPostFetching: false,
 	};
 
-	_createPost (comment) {
+    _setPostsFetchingState(state) {
+        this.setState({
+            isPostsFetching: state,
+        });
+    }
+
+	async _createPost (comment) {
+        this._setPostsFetchingState(true);
 		const post = {
 			id: getUniqueID(),
 			created: moment.utc(),
 			comment,
 		}
 
+        await delay(1200);
+
 		this.setState(({posts}) => ({
 			posts: [post, ...posts],
+            isPostFetching: false,
 		}));
 	}
+
+    async _likePost (id) {
+        const { currentUserFirstName, currentUserLastName } = this.props;
+
+        this._setPostsFetchingState(true);
+
+        await delay(1200);
+
+        const newPosts = this.state.posts.map(post => {
+            if (post.id === id) {
+                return {
+                    ...post,
+                    likes: [
+                        {
+                            id: getUniqueID(),
+                            firstName: currentUserFirstName,
+                            lastName: currentUserLastName,
+                        }
+                    ]
+                }
+            }
+
+            return post;
+        });
+
+        this.setState({
+            posts: newPosts,
+            isPostsFetching: false,
+        });
+    }
+
     render() {
-    	const { posts, isSpinning } = this.state;
+    	const { posts, isPostFetching } = this.state;
 
     	const postsJSX = posts.map((post) => {
     		return <Post 
     			key = { post.id } 
-    			comment = { post.comment }
-    			created = { post.created }
+    			{...post}
+                _likePost = { this._likePost }
 			/>
     	});
         return (
             <section className={Styles.feed}>
-            	<Spinner isSpinning={isSpinning}/>
+            	<Spinner isSpinning={isPostFetching}/>
             	<StatusBar/>
                 <Composer _createPost = { this._createPost } />
                 {postsJSX}
--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -3,19 +3,25 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import Styles from './styles.m.css';
+import { func, string, number, array } from 'prop-types';
 
 // Components
-import { Consumer } from '../HOC/withProfile'; 
+import { Consumer } from '../HOC/withProfile';
+import Like from 'components/Like';
 
 export default class Post extends Component {
     static propTypes  = {
-        comment: PropTypes.string.isRequired,
+        id: string.isRequired,
+        comment: string.isRequired,
+        created: number.isRequired,
+        _likePost: func.isRequired,
+        likes: array.isRequired,
     }
 
     render() {
-        const { comment, created } = this.props;
+        const { comment, created, _likePost, id, likes } = this.props;
 
-        return (
+        return ( 
             <Consumer>
                 {(context) => (
                     <section className = { Styles.post }>
@@ -23,6 +29,12 @@ export default class Post extends Component {
                         <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
                         <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
                         <p>{comment}</p>
+                        <Like 
+                            id = { id } 
+                            likes= { likes } 
+                            _likePost = { _likePost } 
+                            { ...context }
+                        />
                     </section>
                 )}
              </Consumer>
