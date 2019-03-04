271245190811684ddfe6fbd100505e3cb9c3d534
IvanBurlaka <ivanburlakas@gmail.com>
2019-03-04 22:08:20 +0200

    homework_7

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -4,10 +4,9 @@ import moment from 'moment';
 import PropTypes from 'prop-types';
 import Styles from './styles.m.css';
 import Like from '../Like/index';
+import { withProfile } from 'components/HOC/withProfile';
 
-// Components
-import { Consumer } from '../HOC/withProfile'; 
-
+@withProfile
 export default class Post extends Component {
     static propTypes  = {
         comment: PropTypes.string.isRequired,
@@ -15,26 +14,21 @@ export default class Post extends Component {
     }
 
     render() {
-        const { comment, created, _likePost, _deletePost, id, likes } = this.props;
+        const { comment, created, _likePost, _deletePost, id, likes, currentUserFirstName, currentUserLastName, avatar } = this.props;
 
         return (
-            <Consumer>
-                {(context) => (
-                    <section className = { Styles.post }>
-                        <span onClick = { () => { _deletePost(id) } } className = { Styles.cross } />
-                        <img src={ context.avatar } />
-                        <a>{`${context.currentUserFirstName} ${context.currentUserLastName}`}</a>
-                        <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
-                        <p>{comment}</p>
-                        <Like
-                            _likePost = { _likePost }
-                            id = { id }
-                            likes = { likes }
-                            { ...context }
-                        />
-                    </section>
-                )}
-             </Consumer>
+            <section className = { Styles.post }>
+                <span onClick = { () => { _deletePost(id) } } className = { Styles.cross } />
+                <img src={ avatar } />
+                <a>{`${currentUserFirstName} ${currentUserLastName}`}</a>
+                <time>{moment.unix(created).format('MMMM D h:mm:ss a')}</time>
+                <p>{comment}</p>
+                <Like
+                    _likePost = { _likePost }
+                    id = { id }
+                    likes = { likes }
+                />
+            </section>
         );
     }
 }
